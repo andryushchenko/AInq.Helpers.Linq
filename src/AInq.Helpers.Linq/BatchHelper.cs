2eb9aa38e160c802b458ecd94bb1d0d21264c04a
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2021-05-15 15:30:06 +0300

    [ADD] Batch helper

--- src/AInq.Helpers.Linq/BatchHelper.cs
+++ src/AInq.Helpers.Linq/BatchHelper.cs
@@ -12,12 +12,60 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using System.Collections.Generic;
+
 namespace AInq.Helpers.Linq
 {
 
+/// <summary>  Enumerable batch extension </summary>
 public static class BatchHelper
 {
-    
+    /// <summary> Pack the given enumerable in batches with a specified maximum batch size </summary>
+    /// <param name="source"> Source enumerable </param>
+    /// <param name="batchSize"> Maximum batch size </param>
+    /// <typeparam name="T"> Element type </typeparam>
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, long batchSize)
+    {
+        if (batchSize < 1) throw new ArgumentOutOfRangeException(nameof(batchSize));
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+            yield return enumerator.Take(batchSize);
+    }
+
+    /// <summary> Pack the given async enumerable in batches with a specified maximum batch size </summary>
+    /// <param name="source"> Source enumerable </param>
+    /// <param name="batchSize"> Maximum batch size </param>
+    /// <typeparam name="T"> Element type </typeparam>
+    public static async IAsyncEnumerable<IAsyncEnumerable<T>> Batch<T>(this IAsyncEnumerable<T> source, long batchSize)
+    {
+        if (batchSize < 1) throw new ArgumentOutOfRangeException(nameof(batchSize));
+        await using var enumerator = source.GetAsyncEnumerator();
+        while (await enumerator.MoveNextAsync())
+            yield return enumerator.TakeAsync(batchSize);
+    }
+
+    private static IEnumerable<T> Take<T>(this IEnumerator<T> enumerator, long count)
+    {
+        yield return enumerator.Current;
+        count--;
+        while (count > 0 && enumerator.MoveNext())
+        {
+            count--;
+            yield return enumerator.Current;
+        }
+    }
+
+    private static async IAsyncEnumerable<T> TakeAsync<T>(this IAsyncEnumerator<T> enumerator, long count)
+    {
+        yield return enumerator.Current;
+        count--;
+        while (count > 0 && await enumerator.MoveNextAsync())
+        {
+            count--;
+            yield return enumerator.Current;
+        }
+    }
 }
 
 }
